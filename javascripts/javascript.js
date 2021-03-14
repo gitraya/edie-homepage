23237c8fac3905da9c4388facf0d02c3eb5b4f0b
gitraya <mail.raya15@gmail.com>
2021-03-14 11:22:54 +0700

    Update access

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -3,28 +3,31 @@ const closebtn = document.querySelector('.closebtn');
 const openbtn = document.querySelector('.container-nav-mobile');
 const mainContents = document.querySelectorAll('.push-content');
 
+// Function open navigation
 function openNav() {
   closebtn.style.display = 'flex';
   openbtn.style.display = 'none';
-  menuList.style.width = "40%";
-  menuList.style.display = "flex";
-  mainContents.forEach(main => {
+  menuList.style.width = '40%';
+  menuList.style.display = 'flex';
+  mainContents.forEach((main) => {
     main.style.marginRight = '40%';
   });
 }
 
+// Function close navigation
 function closeNav() {
   closebtn.style.display = 'none';
   openbtn.style.display = 'flex';
   menuList.style.width = '0';
   menuList.style.display = 'none';
-  mainContents.forEach(main => {
+  mainContents.forEach((main) => {
     main.style.marginRight = '0';
   });
 }
 
+// Function check size screen and show navigation
 function showNavWeb() {
-    if(window.innerWidth > 760) {
+  if (window.innerWidth > 760) {
     openbtn.style.display = 'none';
     menuList.style.display = 'flex';
     menuList.style.width = 'inherit';
@@ -32,6 +35,6 @@ function showNavWeb() {
     openbtn.style.display = 'flex';
     menuList.style.display = 'none';
   }
-};
+}
 
 window.addEventListener('resize', showNavWeb);
